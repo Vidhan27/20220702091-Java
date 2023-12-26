b02258c9891f979224f98370558c255fd8f6a3ff
Vidhan27 <117517355+Vidhan27@users.noreply.github.com>
2023-12-26 16:32:34 +0530

    Add files via upload

---
+++ DoWhile.java
@@ -0,0 +1,11 @@
+package chapter2;
+
+public class DoWhile{
+	public static void main(String[]args) {
+		int x=0;
+		do {
+			System.out.println(x);
+			x++;
+		}while(x<10);
+	}
+}
---
+++ While.java
@@ -0,0 +1,11 @@
+package chapter2;
+
+public class While{
+	public static void main(String[]args) {
+		int x=0;
+		while(x<10) {
+			System.out.println(x);
+		x++;
+		}
+	}
+}
